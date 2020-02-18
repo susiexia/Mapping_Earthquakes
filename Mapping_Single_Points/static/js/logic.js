6419427d365d2bd09a3b14119d168cd03fdcf2d4
susiexia <57109686+susiexia@users.noreply.github.com>
2020-02-17 21:16:46 -0800

    update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,9 +1,17 @@
 // initialize a map object and set up a center and zoom level
-let mymap = L.map("mapid").setView([40.7, -94.5], 4);
+let mymap = L.map("mapid").setView([37.33, -121.88], 14);
 
 
 // add a marker to map for Bay Area
-var LAmarker = L.marker([37.33, -121.88]).addTo(mymap);
+var SJmarker = L.marker([37.33, -121.88]).addTo(mymap);
+
+// add a circle marker to map: same funtion of L.circle
+var SJcircle = L.circleMarker([37.322752, -121.913344],{
+                        radius: 100, // unit is pixel
+                        color:"red",
+                        fillOpacity:"0.8",
+                        fillColor:"#ffffa1"
+}).addTo(mymap);
 
 
 // create a street tile layer based on Leatlet by mapbox style API 
